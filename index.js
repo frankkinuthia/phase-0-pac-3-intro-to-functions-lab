e3296585f8d407dd0e2dd32eee496b950eabb593
+ <+>
2022-11-17 08:17:36 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,45 @@
+function shout(string) {
+    return string;
+  }
+
+  "Hello!".toUpperCase(); // 'HELLO!'
+
+  function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string) {
+    return string;
+  }
+
+  "Hello!".toLowerCase(); // 'hello!'
+
+  function whisper(string) {
+    return string.toLowerCase ();
+  }
+ 
+    function logShout(string)  {
+        console.log(string.toUpperCase());
+        return string.toUpperCase ();
+  }
+
+    
+  function logWhisper(string)  {
+    console.log(string.toLowerCase());
+    return string.toLowerCase ();
+}
+
+function sayHiToHeadphonedRoommate(string) 
+
+ {   if (string === string.toLowerCase()) {
+      return "I can\'t hear you!";
+    }
+    else if (string === string.toUpperCase()) {
+      return "YES INDEED!";
+    } 
+    else if ('Let\'s have dinner together') {
+    return 'I would love to!';
+    }
+ }
+
+
